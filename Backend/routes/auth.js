9bd4c3e91f0aacfd802909acf022253b6fb3424c
sheraram <sheraram998202@gmail.com>
2022-01-02 01:39:59 +0530

    Hashing Passwords using bcryptjs and add salt to password

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,12 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs'); // password hash and add salt
+
+// Authentication 
+var jwt =require('jsonwebtoken');
+var JWT_SECRET = 'Shera@code@krega';
+
 
 
 // Create a user using : POST "/api/auth/createUser" Doesn't require auth No need to login
@@ -19,19 +25,28 @@ router.post('/createuser', [
         try {
 
             let user = await User.findOne({ email: req.body.email })
-            console.log(user)
-            console.log("HIHIHHHIIHIIHIIIH")
             if (user) {
                 return res.status(400).json({ error: "Sorry a user with this email already exist" })
             }
+            // Password hashing and added salt
+            const salt = await bcrypt.genSaltSync(10);
+            const secpass= await bcrypt.hash(req.body.password, salt);
             // Create user
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password
+                password: secpass
             })
-            console.log(req.body);
-            res.send(user);
+
+            // Authentication 
+            const data={
+                user:{
+                    id: user.id
+                }
+            }
+            const authtoken=jwt.sign(data,JWT_SECRET);
+            console.log(authtoken);
+            res.send(authtoken);
         }
         catch (error) {
             console.error(error.message);
